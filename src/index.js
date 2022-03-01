1c490f9e3f6b91f697d40d7b3ec0584cc8ceb87a
skvoruha <skvoruha@mail.ru>
2022-03-01 18:38:00 +0300

    РёСЃСЂРїР°Р»РІРµРЅР° Р·Р°РїРёСЃС‚ СЃ РЅСѓР»СЏ РїРµСЂРµРґ С‡РёСЃР»РѕРј

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 // в идеале пеередаваемый аргумент должна быть в вызове
-timer('27 february 2022')
+timer('3 march 2022')
 menu()
 modal()
