fdee7cb691ab98dbd35f8a2bde22b5598129f560
skvoruha <skvoruha@mail.ru>
2022-03-04 11:45:05 +0300

    РёСЃРїСЂР°РІРµРЅ 2 РїСѓРЅРєС‚ СЃ РІС‹РІР·РѕРІРј С„СѓРЅРєС†Рё Рѕ РІСЂРµРјСЏ РїСЂРѕСЃР»СѓС€РёРІР°РЅРёСЏ РїРѕ РєР»РёРєСѓ

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -9,30 +9,30 @@ const modal = () =>{
     let idInterval
     let count = 100
 
-    buttons.forEach(btn => {
-      btn.addEventListener('click', () => {
-        modal.style.display = 'block'
-
-        const modalAnimation = () =>{
-          count = count -2
-          idInterval = requestAnimationFrame(modalAnimation)
-          if (count >= 0) {
-            // modal.style.left = count
-            modal.style.transform = 'translateX('+ count +'%)'
-          } else {
-            count = 100
-            cancelAnimationFrame(idInterval)
-          }
-        }
-        modalAnimation()
-
-      })
-    })
-
-    closeBtn.addEventListener('click', ()=>{
+    // функция анимации
+    const modalAnimation = () =>{
+      modal.style.display = 'block'
+      count = count -4
+      idInterval = requestAnimationFrame(modalAnimation)
+      if (count >= 0) {
+        // modal.style.left = count
+        modal.style.transform = 'translateX('+ count +'%)'
+      } else {
+        count = 100
+        cancelAnimationFrame(idInterval)
+      }
+    }
+    // функция закрытия анимации
+    const modalAnimationClose = () =>{
       modal.style.transform = 'translateX(100%)'
       modal.style.display = 'none'
+    }
+
+    buttons.forEach(btn => {
+      btn.addEventListener('click', modalAnimation)
     })
+
+    closeBtn.addEventListener('click', modalAnimationClose)
   }
 }
 
