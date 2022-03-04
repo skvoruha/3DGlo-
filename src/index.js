4aaa399d88f2d7dcce1d1073d009b8ee1de1957c
skvoruha <skvoruha@mail.ru>
2022-03-04 13:50:17 +0300

    РїРµСЂРµРЅС‘СЃ РєРѕРґ СЃ 20 СѓСЂРѕРєР°

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import form from './modules/form'
 
 // в идеале пеередаваемый аргумент должна быть в вызове
 // timer('28 february 2022')
-timer('3 march 2022')
+timer('9 march 2022')
 menu()
 modal()
 calc()
