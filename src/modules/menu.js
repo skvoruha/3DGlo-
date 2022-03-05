62ec43bcf76632f870496e240f188da51bc0ce6d
skvoruha <skvoruha@mail.ru>
2022-03-05 17:24:02 +0300

    completed the main task of lesson 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,16 @@
 const menu = () =>{
+  // кнопка для открытия меню
   const menuBtn = document.querySelector('.menu')
+  // само меню
   const menu = document.querySelector('menu')
-  const closeBtn = menu.querySelector('.close-btn')
-  const menuItems = menu.querySelectorAll('ul > li > a')
 
   const handleMenu = (e) => {
     e.preventDefault()
     menu.classList.toggle('active-menu')
     if (e.target.matches('ul > li > a')) {
+      // присваиванием href то есть ссылку к какому id перейти экрану
       let elmnt = document.querySelector(e.target.hash)
-      // elmnt.scrollIntoView();
+      // экран с настройками переходт к элементу по id
       elmnt.scrollIntoView({
         block: "start",
         behavior: "smooth"
@@ -18,13 +19,17 @@ const menu = () =>{
   }
   // передаём по клику нукцию handleMenu
   menuBtn.addEventListener('click', handleMenu)
-  closeBtn.addEventListener('click', handleMenu)
+  // closeBtn.addEventListener('click', handleMenu)
 
-  for (let i = 0; i < menuItems.length -1;i++){
-    menuItems[i].addEventListener('click', handleMenu)
-  }
-
-  menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+  menu.addEventListener('click', (e)=>{
+    // в e -> target  ищем classList -> conatins (проверяем есть ли во вложенномти элемент с таким классом )
+    if(e.target.classList.contains('close-btn')) {
+      handleMenu(e)
+      // проверяем matches вложеннные теги и классы,
+    } else if (e.target.matches('ul > li > a')){
+      handleMenu(e)
+    }
+  })
 }
 
 export default menu
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -24,32 +24,11 @@ const modal = () =>{
         cancelAnimationFrame(idInterval)
       }
     }
-    // функция закрытия анимации
-    const modalAnimationClose = () =>{
-      modal.style.transform = 'translateX(100%)'
-      modal.style.display = 'none'
-    }
     // перебор кнопок nodelist
     buttons.forEach(btn => {
       btn.addEventListener('click', modalAnimation)
     })
-
-    closeBtn.addEventListener('click', modalAnimationClose)
-      btnScroll.addEventListener('click',(e)=>{
-      e.preventDefault()
-      // получаем высоту где находится наш экран
-      let top = document.documentElement.scrollTop
-      const scrollBottom = () =>{
-        top = top + 20
-        idScroll = requestAnimationFrame(scrollBottom)
-        document.documentElement.scrollTop = top
-        // если  top больше 850 то сбрасываем анимацию
-        if(top > 850) {
-          cancelAnimationFrame(idScroll)
-        }
-      }
-      scrollBottom()
-    })
+    // закрытия анимациии
     modal.addEventListener('click', (e)=>{
       if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
         modal.style.display = 'none'
