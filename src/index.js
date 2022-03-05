91c8e1f27993116e3e1e469250d87847ed098ad1
skvoruha <skvoruha@mail.ru>
2022-03-05 13:30:48 +0300

    РїСЂРѕСЃС‚Рѕ

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import modal from './modules/modal'
 
 // в идеале пеередаваемый аргумент должна быть в вызове
 // timer('28 february 2022')
-timer('1 march 2022')
+timer('20 march 2022')
 menu()
 modal()
