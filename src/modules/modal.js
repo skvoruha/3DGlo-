1c4e3a01cfc002cb7a56e8f39aba3772ed7ed755
skvoruha <skvoruha@mail.ru>
2022-03-07 22:13:36 +0300

    added screen width

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -26,7 +26,11 @@ const modal = () =>{
     }
     // перебор кнопок nodelist
     buttons.forEach(btn => {
-      btn.addEventListener('click', modalAnimation)
+      btn.addEventListener('click', ()=>{
+        if (window.innerWidth > 767) {
+          modalAnimation()
+        }
+      })
     })
     // закрытия анимациии
     modal.addEventListener('click', (e)=>{
