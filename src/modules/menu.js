c72a4ceec041e8a2c90a281d56cd74af3b68c019
skvoruha <skvoruha@mail.ru>
2022-03-09 17:11:11 +0300

    add menu since 23 lesson

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -18,10 +18,10 @@ const menu = () =>{
 
   document.addEventListener('click',(e)=>{
     // в e -> target  ищем classList -> conatins (проверяем есть ли во вложенномти элемент с таким классом )
-    if( e.target.closest('.close-btn') || !e.target.matches('menu') ||
-        e.target.matches('ul > li > a') || (!e.target.closest('.active-menu') && menu.classList.contains('active-menu'))) {
+
+    if( e.target.closest('.close-btn') || e.target.matches('ul > li > a') ||
+    e.target.closest('.menu') || (!e.target.closest('.active-menu') && menu.classList.contains('active-menu'))) {
       handleMenu(e)
-      // проверяем matches вложеннные теги и классы,
     }
   })
 }
